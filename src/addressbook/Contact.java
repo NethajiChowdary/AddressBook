e19a4bf84f8c2ba6f56cf5431eea896eda626819
NethajiChowdary <nethajichowdary555@gmail.com>
2021-07-16 00:23:23 +0530

    Added: Code to Search Person in a City or State Across The Multiple AddressBook

--- src/addressbook/Contact.java
+++ src/addressbook/Contact.java
@@ -1,106 +1,94 @@
 package addressbook;
 
-public class Contact
-{
+public class Contact {
 
-		private String firstName, lastName, address, city, state, email, zip, phoneNumber;
+	String firstname;
+	String lastname;
+	String city;
+	String state;
+	int zipcode;
+	String phonenumber;
+	String email;
+	
+	public Contact(String firstname, String lastname, String city, String state, int zipcode,
+			String phonenumber,String email) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.city = city;
+		this.state = state;
+		this.zipcode = zipcode;
+		this.phonenumber = phonenumber;
+		this.email = email;
+	}
 
-		public Contact(String firstName, String lastName, String address, String city, String state, String email,
-				String phoneNumber, String zip)
-		{
-			setFirstName(firstName);
-			setLastName(lastName);
-			setAddress(address);
-			setCity(city);
-			setState(state);
-			setEmail(email);
-			setPhoneNumber(phoneNumber);
-			setZip(zip);
 
-		}
+	public Contact() {
+		// TODO Auto-generated constructor stub
+	}
 
-		public Contact()
-		{
 
-		}
+	public String getFirstname() {
+		return firstname;
+	}
 
-		public String getFirstName() 
-		{
-			return firstName;
-		}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
 
-		public void setFirstName(String firstName) 
-		{
-			this.firstName = firstName;                                           
-		}
+	public String getLastname() {
+		return lastname;
+	}
 
-		public String getLastName() 
-		{
-			return lastName;
-		}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
 
-		public void setLastName(String lastName)
-		{
-			this.lastName = lastName;
-		}
+	public String getCity() {
+		return city;
+	}
 
-		public String getAddress()
-		{
-			return address;
-		}
+	public void setCity(String city) {
+		this.city = city;
+	}
 
-		public void setAddress(String address) 
-		{
-			this.address = address;
-		}
+	public String getState() {
+		return state;
+	}
 
-		public String getCity() 
-		{
-			return city;
-		}
+	public void setState(String state) {
+		this.state = state;
+	}
 
-		public void setCity(String city) 
-		{
-			this.city = city;
-		}
+	public int getZipcode() {
+		return zipcode;
+	}
 
-		public String getState()
-		{
-			return state;
-		}
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
 
-		public void setState(String state) 
-		{
-			this.state = state;
-		}
+	public String getPhonenumber() {
+		return phonenumber;
+	}
 
-		public String getEmail() 
-		{
-			return email;
-		}
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	
+	public String getEmail() {
+		return email;
+		
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-		public void setEmail(String email)
-		{
-			this.email = email;
-		}
+	@Override
+	public String toString() {
+		return "Contact [firstname=" + firstname + ", lastname=" + lastname + ", city=" + city + ", state="
+				+ state + ", zipcode=" + zipcode + ", phonenumber=" + phonenumber + ",email="+email+"]";
+	}
 
-		public String getZip()
-		{
-			return zip;
-		}
-
-		public void setZip(String zip) 
-		{
-			this.zip = zip;
-		}
-
-		public String getPhoneNumber()
-		{
-			return phoneNumber;
-		}
-
-		public void setPhoneNumber(String phoneNumber) 
-		{
-			this.phoneNumber = phoneNumber;
-		}
 }
