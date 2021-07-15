e6575739e999de92ea78af36b2e36037f73c1535
NethajiChowdary <nethajichowdary555@gmail.com>
2021-07-16 00:42:26 +0530

    Added: Code to View Persons by City or State

--- src/addressbook/Contact.java
+++ src/addressbook/Contact.java
@@ -1,6 +1,7 @@
 package addressbook;
 
-public class Contact {
+public class Contact
+{
 
 	String firstname;
 	String lastname;
@@ -11,7 +12,8 @@ public class Contact {
 	String email;
 	
 	public Contact(String firstname, String lastname, String city, String state, int zipcode,
-			String phonenumber,String email) {
+			String phonenumber,String email) 
+	{
 		super();
 		this.firstname = firstname;
 		this.lastname = lastname;
@@ -22,71 +24,84 @@ public class Contact {
 		this.email = email;
 	}
 
-
-	public Contact() {
-		// TODO Auto-generated constructor stub
+	public Contact()
+	{
 	}
 
-
-	public String getFirstname() {
+	public String getFirstname()
+	{
 		return firstname;
 	}
 
-	public void setFirstname(String firstname) {
+	public void setFirstname(String firstname) 
+	{
 		this.firstname = firstname;
 	}
 
-	public String getLastname() {
+	public String getLastname() 
+	{
 		return lastname;
 	}
 
-	public void setLastname(String lastname) {
+	public void setLastname(String lastname)
+	{
 		this.lastname = lastname;
 	}
 
-	public String getCity() {
+	public String getCity()
+	{
 		return city;
 	}
 
-	public void setCity(String city) {
+	public void setCity(String city) 
+	{
 		this.city = city;
 	}
 
-	public String getState() {
+	public String getState()
+	{
 		return state;
 	}
 
-	public void setState(String state) {
+	public void setState(String state) 
+	{
 		this.state = state;
 	}
 
-	public int getZipcode() {
+	public int getZipcode() 
+	{
 		return zipcode;
 	}
 
-	public void setZipcode(int zipcode) {
+	public void setZipcode(int zipcode)
+	{
 		this.zipcode = zipcode;
 	}
 
-	public String getPhonenumber() {
+	public String getPhonenumber()
+	{
 		return phonenumber;
 	}
 
-	public void setPhonenumber(String phonenumber) {
+	public void setPhonenumber(String phonenumber)
+	{
 		this.phonenumber = phonenumber;
 	}
 	
-	public String getEmail() {
+	public String getEmail() 
+	{
 		return email;
 		
 	}
 	
-	public void setEmail(String email) {
+	public void setEmail(String email) 
+	{
 		this.email = email;
 	}
 
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		return "Contact [firstname=" + firstname + ", lastname=" + lastname + ", city=" + city + ", state="
 				+ state + ", zipcode=" + zipcode + ", phonenumber=" + phonenumber + ",email="+email+"]";
 	}
